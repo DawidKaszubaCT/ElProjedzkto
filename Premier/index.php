26d177cbbeb4efe72fcb2c8ed28ccbfdc6870e0b
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2025-03-20 15:38:03 +0100

    Main layout

--- Premier/index.php
+++ Premier/index.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <aside>
+        <a href="index.php">Strona główna</a>
+    </aside>
+
+     <section>
+
+     </section>
+     
+    <footer>
+        <h2>Copyright by Super sigmas!</h2>
+    </footer>
 </body>
 </html>
