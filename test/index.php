780c20a6275eebffc046d88aef2af2ec06f71bb2
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2025-03-21 19:49:17 +0100

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+
+    <!--aside - menu po lewej -->
+
+    <aside>
+        <a href="index.php">Strona główna</a>
+    </aside>
+
+    <!--section - główna część strony-->
+    <article id="article-glowna">
+    <section id="glowna">
+        <section class="blok">
+        <h2>bla bla bla</h2>
+        </section>
+        <section class="blok">
+        <h2>bla bla bla</h2>
+        </section>
+        <section class="blok">
+        <h2>bla bla bla</h2>
+        </section>
+        <section class="blok">
+        <h2>bla bla bla</h2>
+        </section>
+        <section class="blok">
+        <h2>bla bla bla</h2>
+        </section>
+        <section class="blok">
+        <h2>bla bla bla</h2>
+        </section>
+        
+    </section>
+</article>
+    </main>
+
+
+
+    <footer>
+        <h2>Copyright by Super sigmas!</h2>
+    </footer>
+</body>
+</html>
